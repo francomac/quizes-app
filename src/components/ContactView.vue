f4f7e896bb3376683216e89f146fa6a0717a9700
francomac <franco.mcarballo@outlook.com>
2024-05-10 19:05:16 -0600

    contact view file for quizes app

---
+++ src/components/ContactView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import quizData from '@/data/quizesData.json'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const quiz = quizData.find((quiz) => quiz.id === Number(route.params.id))
+</script>
+
+<template>
+  <div v-if="quiz">
+    <h1>Contact info</h1>
+    <h2>{{ quiz.contact.name }}</h2>
+    <p>{{ quiz.contact.email }}</p>
+    <p>{{ quiz.contact.phone }}</p>
+  </div>
+  <div v-else>
+    <h1>Quiz not found</h1>
+  </div>
+</template>
