9a8dd7bf29b9ee2a0a3588f8cd1c80091548d95c
francomac <franco.mcarballo@outlook.com>
2024-05-12 09:54:35 -0600

    adding quiz questions into quizes app

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,16 +1,10 @@
 <script setup>
 import { useRoute, useRouter, RouterLink, RouterView } from 'vue-router'
 import quizData from '@/data/quizesData.json'
+import { ref } from 'vue'
+import QuestionsList from '@/components/QuizesQuestionsList.vue'
+
 // import { computed, onMounted, watch } from 'vue'
-
-const route = useRoute()
-const router = useRouter()
-
-const quiz = quizData.find((quiz) => quiz.id === Number(route.params.id))
-
-const goBackHome = () => {
-  router.push('/')
-}
 
 // const quizStatus = computed(() => {
 //   console.log('quiz', quiz)
@@ -30,6 +24,21 @@ const goBackHome = () => {
 //     router.push({ name: 'NotFound' })
 //   }
 // })
+
+const route = useRoute()
+const router = useRouter()
+
+const quiz = quizData.find((quiz) => quiz.id === Number(route.params.id))
+
+const goBackHome = () => {
+  router.push('/')
+}
+
+const selectedQuestions = ref([])
+const onSelectQuestion = (questionsToUse) => {
+  console.log('onSelectQuestion', questionsToUse)
+  selectedQuestions.value = questionsToUse
+}
 </script>
 
 <template>
@@ -42,6 +51,9 @@ const goBackHome = () => {
         <p>{{ quiz.count }} Questions</p>
       </div>
     </div>
+
+    <QuestionsList @selectQuestion="onSelectQuestion" :questions="quiz" />
+    <br />
 
     <RouterLink :to="'/quiz/' + quiz.id + '/contact'">
       <button>Show Contact</button>
@@ -59,7 +71,7 @@ const goBackHome = () => {
   width: 310px;
   overflow: hidden;
   border-radius: 2%;
-  /* box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1); */
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
   margin-bottom: 35px;
   margin-right: 20px;
   cursor: pointer;
